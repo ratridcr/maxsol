8abeb9ab22fde8bebf11cd1beaeb8e87367038ec
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-01-21 18:40:16 +0800

    Pengerjaan sampai banner, web company profile maxsol

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PT. Maxsolusindo Jaya</title>
+    <link rel="shortcut icon" type="image/png" href="img/icon.png">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- --- awal-header --- -->
+    <header class="header">
+        <div class="header__logo-box">
+            <img src="img/logo.png" alt="Logo-Maxsol">
+        </div>
+        <div class="header__text-box">
+            text
+        </div>
+        <div class="header__img-box">
+            <img src="img/header.png" alt="" class="header__img-1">
+            <img src="img/banner.png" alt="" class="header__img-2">
+        </div>
+    </header>
+    <!-- --- akhir-header --- -->
+</body>
+</html>
