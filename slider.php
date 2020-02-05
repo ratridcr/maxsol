1b6e325cdbb2f795ac617b8475a1635e83662f8f
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-02-05 13:16:43 +0800

    responsive mobile, tab, dekstop kelarr

--- slider.php
+++ slider.php
@@ -101,14 +101,32 @@
 <script src="swiper/js/swiper.min.js"></script>
 
 <script>
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        navigation: {
+
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      }
     });
+
+    
 </script>
 </body>
 </html>
