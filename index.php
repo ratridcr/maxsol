c9a02b4b7064cf0df52f97f67f47e67e0d7ce2c3
natanael kristiawan <natanaelkristiawan@hotmail.com>
2020-02-06 17:25:30 +0800

    nambahin navigation, loader, dan animasi

--- index.php
+++ index.php
@@ -13,6 +13,38 @@
 </head>
 <body>
 
+    <!-- --- awal loader --- -->
+    <div class="preloader">
+        <div class="loader">
+            <div class="loader__circle"></div>
+            <div class="loader__circle"></div>
+        </div>
+    </div>
+    <!-- --- akhir loader --- -->
+
+    <!-- --- awal navigation --- -->
+    <div class="navigation">
+        <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+
+        <label for="navi-toggle" class="navigation__button">
+            <span class="navigation__icon">&nbsp;</span>
+        </label>
+
+        <div class="navigation__background">&nbsp;</div>
+
+        <nav class="navigation__nav">
+            <ul class="navigation__list">
+                <li class="navigation__item active"><a href="#" class="navigation__link"><span>01</span>home</a></li>
+                <li class="navigation__item"><a href="#about" class="navigation__link"><span>02</span>history</a></li>
+                <li class="navigation__item"><a href="#service" class="navigation__link"><span>03</span>Services</a></li>
+                <li class="navigation__item"><a href="#project" class="navigation__link"><span>04</span>project</a></li>
+                <li class="navigation__item"><a href="#team" class="navigation__link"><span>05</span>Team</a></li>
+                <li class="navigation__item"><a href="#contact" class="navigation__link"><span>06</span>Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- --- akhir navigation --- -->
+
     <!-- --- awal-header --- -->
     <header class="header">
         <div class="header__logo-box">
@@ -21,7 +53,7 @@
         <div class="header__text-box">
             <h1 class="heading-primary u-margin-bottom-medium">
                 <span class="heading-primary--main">pride.</span>
-                <span class="heading-primary--main">creative.</span>
+                <span class="heading-primary--sub">creative.</span>
                 <span class="heading-primary--main">in every pixel.</span>
             </h1>
             <a href="#" class="btn btn--blue btn--animated">Load more</a>
@@ -36,7 +68,7 @@
     <main>
 
         <!-- --- awal-about --- -->
-        <section class="section-about">
+        <section class="section-about" id="about">
 
             <div class="row">
                 <div class="col-1-of-2">
@@ -72,7 +104,7 @@
 
 
         <!-- --- awal-service --- -->
-        <section class="section-service">
+        <section class="section-service" id="service">
             <div class="row">
                 <div class="col-1-of-2">
                     <div class="u-center-text">
@@ -127,7 +159,7 @@
         <!-- --- akhir-service --- -->
 
         <!-- --- awal-project --- -->
-        <section class="section-project">
+        <section class="section-project" id="project">
             <div class="u-center-text">
                 <h2 class="heading-secondary u-margin-bottom-small">
                     Our projects
@@ -296,7 +328,7 @@
         <!-- --- akhir-project --- -->
 
         <!-- --- awal team --- -->
-        <section class="section-team">
+        <section class="section-team" id="team">
             <div class="team u-center-text">
                 <h2 class="heading-secondary u-margin-bottom-small">
                     Our team
@@ -312,7 +344,7 @@
         <!-- --- akhir team --- -->
 
         <!-- --- awal contact --- -->
-        <section class="section-contact">
+        <section class="section-contact" id="contact">
             <div class="u-center-text">
                 <h2 class="heading-secondary u-margin-bottom-medium">
                     Contact Us
@@ -344,7 +376,8 @@
         </section>
         <!-- --- akhir contact --- -->
     </main>
-
+    
+    <!-- --- awal footer --- -->
     <footer class="footer">
         <div class="footer__logo-box">
             <img src="img/logo.png" alt="Full logo" class="footer__logo">
@@ -391,6 +424,7 @@
             </div>
         </div>
     </footer>
+    <!-- --- akhir footer --- -->
 
 <!-- 
     <div class="row">
@@ -420,9 +454,12 @@
             col 1 of 2
         </div>
     </div> -->
-
+        
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
         <script src="font-awesome/js/all.min.js"></script>
         <script src="swiper/js/swiper.min.js"></script>
+    
+    <!-- --- awal-slider --- -->
     <script>
         var swiper = new Swiper('.swiper-container', {
             slidesPerView: 1,
@@ -437,5 +474,24 @@
             
         });
     </script>
+    <!-- --- akhir-slider --- -->
+
+    <!-- --- awal-menu-active --- -->
+    <script>
+        $(document).on('click', 'ul li', function(){
+            $(this).addClass('active').siblings().removeClass('active')
+        })
+    </script>
+    <!-- --- akhir-menu-active --- -->
+
+    <!-- --- awal-loader --- -->
+    <script>
+        $(document).ready(function(){
+            setTimeout(function() {
+                $(".preloader").fadeOut();
+            }, 4000);
+        })
+    </script>
+    <!-- --- awal-loader --- -->
 </body>
 </html>
